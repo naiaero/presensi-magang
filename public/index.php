e1e33bff77178e28e0d9648e5c6820d5be0793e2
naiaero <salsabilanailafahdi26@gmail.com>
2026-08-05 11:37:02 +0800

    perbaiki tp ga baik baik

--- public/index.php
+++ public/index.php
@@ -6,15 +6,28 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../laravel_core/storage/framework/maintenance.php')) {
+    require $maintenance;
+} elseif (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+if (file_exists(__DIR__.'/../laravel_core/vendor/autoload.php')) {
+    require __DIR__.'/../laravel_core/vendor/autoload.php';
+} else {
+    require __DIR__.'/../vendor/autoload.php';
+}
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+if (file_exists(__DIR__.'/../laravel_core/bootstrap/app.php')) {
+    $app = require_once __DIR__.'/../laravel_core/bootstrap/app.php';
+} else {
+    $app = require_once __DIR__.'/../bootstrap/app.php';
+}
+
+// Automatically set the public path to whatever directory this index.php is in (e.g. public_html)
+$app->usePublicPath(__DIR__);
 
 $app->handleRequest(Request::capture());
